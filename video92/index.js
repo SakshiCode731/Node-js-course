72e5311f4f02f291fc4420758d5ba3e530838773
SakshiCode731 <sakshigautam90111@gmail.com>
2026-03-19 00:32:37 +0530

    add the code

--- video92/index.js
+++ video92/index.js
@@ -8,15 +8,10 @@ app.get('/', (req, res) => {
   let sitename = "Adidas"
   let serchText = "Search Now"
   let arr = ["hello", 2, 3, 4, 5]
-  res.render('index', { sitename: sitename, serchText: serchText,  arr })
-})
 
-app.get('/blog/:slug', (req, res) => {
-  let blogtitle = "Adidas why and when?"
-  let blogcontent = "its a sports brand"
-  res.render('blogpost', { blogtitle: blogtitle, blogcontent: blogcontent })
+  res.render('index', { sitename, serchText, arr })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
