ba80ae1b8c52ddd9c52602dfcfb46790536f9821
SakshiCode731 <sakshigautam90111@gmail.com>
2026-03-21 23:21:09 +0530

    this is my code

---
+++ video96/main.js
@@ -0,0 +1,14 @@
+// Generate a dummy data in this format in a collection called employees in a 
+// db called company
+
+//{
+// name: :"sakshi",
+// salary: 50000,
+// langauge: "python",
+// city:"New York",
+// isManager: true
+// }
+
+// Generate 10 such document when a button called genearte data is clicked!
+// Create an Express app with mongoose to achieve it
+// Everytime the button is clicked, you should clear the collection
