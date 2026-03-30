477ad345359fd7fa773ed6db87eab2dc31ef5e3e
SakshiCode731 <sakshigautam90111@gmail.com>
2026-03-30 23:40:02 +0530

    adding my exercise code here

---
+++ video100/main.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {foo: 'FOO'});
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ video100/question.md
@@ -0,0 +1,12 @@
+<!-- Genarate a dummy in this format in a collection called employees in a db called company -->
+
+<!--{
+name: "Sakshi",
+salary: "45000000000",
+languge: "python",
+city: "New York",
+is Manager: "true
+} 
+
+Generate 10 such needs when a button called generate data is clicked!
+-->
