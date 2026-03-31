3e643def72239b91442c1248fa4d4a863fede1d1
SakshiCode731 <sakshigautam90111@gmail.com>
2026-03-31 23:12:10 +0530

    adding practice code

--- video100/main.js
+++ video100/main.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
+const Employee = require('./models/Employee.js');
+
+mongoose.connect('mongodb://127.0.0.1:27017/company');
 const port = 3000
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.render('index', {foo: 'FOO'});
-});
+const getRandom = (arr)=>{
+  let rno = Math.floor(Math.random() * arr.length - 1);
+  return arr[rno]
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render('index', { foo: 'FOO' });
+});
+
+app.get('/generate', async (req, res) => {
+  //generate random data
+
+  let randomNames = ["Sakshi", "Rahul", "Anjali", "priya"]
+  let randomLang = ["JavaScript", "Python", "Java", "C++"]
+  let randomCities = ["Bilaspur", "Moradabad", "Mysore", "kolkata"]
+  for (let index = 0; index < 10; index++) {
+    let e = await Employee.create({
+      name: "Sakshi",
+      salary: 450000,
+      language: "JavaScript",
+      city: "New York",
+      isManager: true
+    })
+
+   console.log(e);
+
+  }
+  res.render('index', { foo: 'FOO' });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
